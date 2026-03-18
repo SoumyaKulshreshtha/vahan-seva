a6d77d826535a4dacefd50a505152957b62db878
Soumya Kulshreshtha <46748675+SoumyaKulshreshtha@users.noreply.github.com>
2026-03-18 12:27:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ db.serialize(() => {
       location TEXT,
       vehicle_type TEXT,
       created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+      price_service TEXT DEFAULT '0',
+      price_wash TEXT DEFAULT '0',
     )
   `);
 
