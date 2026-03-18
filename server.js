f9d5fbb6dd68e1492c1d7b9dfcb6b58192645ca0
Soumya Kulshreshtha <46748675+SoumyaKulshreshtha@users.noreply.github.com>
2026-03-18 12:51:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ const db = new sqlite3.Database("./vahanseva.db");
 
 // ---------------------- DB INIT ----------------------
 db.serialize(() => {
+    db.run(`DROP TABLE IF EXISTS mechanic_documents`);
+    db.run(`DROP TABLE IF EXISTS mechanics`);
+    db.run(`DROP TABLE IF EXISTS bookings`);
+    db.run(`DROP TABLE IF EXISTS users`);
+    
     db.run(`
     CREATE TABLE IF NOT EXISTS users (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
